be91e4b2ef606ee67eeb1db48d31b0ac3f3ae471
SAYYED FAIYAZ AHAMED <sayyedfaiyaz270907@gmail.com>
2026-06-10 15:13:35 +0530

    Implement file handling operations in main.py
    Added file handling operations including creating, reading, renaming, and moving files.

--- Task1_File_Handling/main.py
+++ Task1_File_Handling/main.py
@@ -1,6 +1,28 @@
-try:
-    with open("abc.txt", "r") as file:
-        print(file.read())
+import os
+import shutil
+import csv
 
-except FileNotFoundError:
-    print("File not found")
+# Create text file
+with open("data.txt", "w") as file:
+    file.write("Hello Internship")
+
+# Read file
+with open("data.txt", "r") as file:
+    print(file.read())
+
+# Create CSV file
+with open("students.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Name", "Age"])
+    writer.writerow(["Faiyaz", 18])
+
+# Create backup folder
+os.makedirs("backup", exist_ok=True)
+
+# Rename file
+os.rename("data.txt", "new_data.txt")
+
+# Move file
+shutil.move("new_data.txt", "backup/new_data.txt")
+
+print("Task completed successfully")
